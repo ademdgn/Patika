746ce7b5a4d64bebadb015d1cea21953e7688f9f
ademdgn <ademdogan972@gmail.com>
2023-03-25 12:46:35 +0300

    divide

---
+++ patika_divide/divide.cs
@@ -0,0 +1,46 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        
+        Console.WriteLine(((float)arr.Count(x => x > 0) / arr.Count()).ToString("0.000000"));
+ Console.WriteLine(((float)arr.Count(x => x < 0) / arr.Count()).ToString("0.000000"));
+ Console.WriteLine(((float)arr.Count(x => x == 0) / arr.Count()).ToString("0.000000"));
+        
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
