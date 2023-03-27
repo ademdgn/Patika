90da72e7c5d1ea711f5f4627e7a918cb87c0f624
ademdgn <ademdogan972@gmail.com>
2023-03-27 15:02:20 +0300

    .

--- List/HW3-/obj/Debug/net7.0/HW3-.AssemblyInfo.cs
+++ List/HW3-/obj/Debug/net7.0/HW3-.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HW3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("HW3-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("HW3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HW3")]
+[assembly: System.Reflection.AssemblyProductAttribute("HW3-")]
+[assembly: System.Reflection.AssemblyTitleAttribute("HW3-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
