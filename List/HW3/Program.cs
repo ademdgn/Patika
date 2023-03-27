44312f2af32468b6214804ccea4c73ba543781b8
ademdgn <ademdogan972@gmail.com>
2023-03-27 15:00:40 +0300

    up

--- List/HW3/Program.cs
+++ List/HW3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+public class Hw2{
+    public static void Main(String[] args){
+        Console.Write("Bir cümle giriniz ..");
+        string cumle = Console.ReadLine();
+        char[] sesli = {'a','e','ı','i','o','ö','u','ü'};
+
+        ArrayList dizi = new ArrayList();
+
+        if(!string.IsNullOrEmpty(cumle)){
+            foreach (char item in cumle)
+            {
+                foreach (char sesliler in sesli)
+                {
+                    if(item == sesliler){
+                        dizi.Add(sesliler);
+                    }
+                }
+            }
+        }
+        dizi.Sort();
+        for (int i = 0; i < dizi.Count; i++)
+        {
+            Console.WriteLine(dizi[i]);
+        }
+
+     }
+}
